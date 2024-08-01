96f50c3b65b5fc70d202a16851dd34968ac665a1
Matias-sh <devmatiasbritez@gmail.com>
2024-08-01 13:05:27 -0300

    small changes to the login layout

--- app/src/main/java/com/example/rafapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/rafapp/activities/LoginActivity.kt
@@ -1,6 +1,10 @@
 package com.example.rafapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +20,26 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Iniciamos los componentes
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        // Configuramos el listener para el botón de login
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username == "admin" && password == "1234") {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                // Init main activity
+                val intentMain = Intent(this, MainActivity::class.java)
+                startActivity(intentMain)
+            } else {
+                Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
